e049dcc32005cc8745d07bfa0338cad9d25cd48f
Gaurav Trivedi <trivedi.gaurav936@gmail.com>
2026-06-24 02:52:15 +0530

    removed all files

--- While_loop.py
+++
@@ -1,9 +0,0 @@
-from loguru import logger
-labour_name = ['Mahesh','Ramesh','Mithlesh','Sumesh']
-import time
-
-name_iter = 0
-while(name_iter<len(labour_name)):
-    print(labour_name[name_iter])
-    time.sleep(4)
-    name_iter+=1
